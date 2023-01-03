5b8583df26ba0415a312b2e08512dda20b06a3b7
LeandroDoudement <leandrodoudement@gmail.com>
2023-01-03 02:09:48 -0300

    Correção de erro em que se não tinha nada no localStorage, Cart crashava

--- game-ecommerce/src/pages/Cart.js
+++ game-ecommerce/src/pages/Cart.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
 import { faMinus } from '@fortawesome/free-solid-svg-icons';
@@ -10,6 +10,9 @@ import '../styles/Cart.css';
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
   useEffect(() => {
+    if (!localStorage.getItem('games')) {
+      localStorage.setItem('games', JSON.stringify([]));
+    }
     setCartItems(JSON.parse(localStorage.getItem('games')));
   }, []);
 
@@ -44,50 +47,54 @@ const Cart = () => {
       <Header />
       <div className='cart-page'>
         <div className='cart-and-payment-wrapper'>
-        <div className='cart-box'>
-          <span className='cart-title'>Carrinho de compras</span>
-          {cartItems.length === 0 ? (
-            <span className='empty-cart'>Seu carrinho está vazio</span>
-          ) : (
-            cartItems.map((item, index) => (
-              <div className='item-wrapper'>
-                <div className='item' key={index}>
-                  <FontAwesomeIcon
-                    icon={faX}
-                    className='delete-product'
-                    onClick={() => removeCartItem(item)}
-                  />
-                  <img
-                    src={require(`../images/${item.image}`)}
-                    alt={item.name}
-                    className='item-img'
-                  />
-                  <span className='item-name'>{item.name}</span>
-                  <FontAwesomeIcon
-                    icon={faMinus}
-                    onClick={() => updateQuantity(index, '-')}
-                  />
-                  <span className='item-quantity'>{item.quantity}</span>
-                  <FontAwesomeIcon
-                    icon={faPlus}
-                    onClick={() => updateQuantity(index, '+')}
-                  />
-                  <span>{`R$${(item.price * item.quantity).toFixed(2)}`}</span>
+          <div className='cart-box'>
+            <span className='cart-title'>Carrinho de compras</span>
+            {cartItems.length === 0 ? (
+              <span className='empty-cart'>Seu carrinho está vazio</span>
+            ) : (
+              cartItems.map((item, index) => (
+                <div className='item-wrapper'>
+                  <div className='item' key={index}>
+                    <FontAwesomeIcon
+                      icon={faX}
+                      className='delete-product'
+                      onClick={() => removeCartItem(item)}
+                    />
+                    <img
+                      src={require(`../images/${item.image}`)}
+                      alt={item.name}
+                      className='item-img'
+                    />
+                    <span className='item-name'>{item.name}</span>
+                    <FontAwesomeIcon
+                      icon={faMinus}
+                      onClick={() => updateQuantity(index, '-')}
+                    />
+                    <span className='item-quantity'>{item.quantity}</span>
+                    <FontAwesomeIcon
+                      icon={faPlus}
+                      onClick={() => updateQuantity(index, '+')}
+                    />
+                    <span>{`R$${(item.price * item.quantity).toFixed(
+                      2
+                    )}`}</span>
+                  </div>
+                  <hr />
                 </div>
-                <hr />
-              </div>
-            ))
-          )}
-        </div>
-        <div className='total-price-box'>
-          <span>{`${cartItems.length} items: R$${totalPrice.toFixed(2)}`}</span>
-          <span>
-            {totalPrice > 250
-              ? `Frete Gratis!`
-              : `Frete: ${(cartItems.length * 10).toFixed(2)}`}
-          </span>
-          <button className='payment-button'>Finalizar compra</button>
-        </div>
+              ))
+            )}
+          </div>
+          <div className='total-price-box'>
+            <span>{`${cartItems.length} items: R$${totalPrice.toFixed(
+              2
+            )}`}</span>
+            <span>
+              {totalPrice > 250
+                ? `Frete Gratis!`
+                : `Frete: ${(cartItems.length * 10).toFixed(2)}`}
+            </span>
+            <button className='payment-button'>Finalizar compra</button>
+          </div>
         </div>
       </div>
     </div>
